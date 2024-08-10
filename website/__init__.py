7c51062c069a9eebd8524c6651362743d4dd7014
PaoloBaltazar <gpaolobaltazar@gmail.com>
2024-08-10 20:52:49 +0800

    setting up routes

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 def create_app():
   app = Flask(__name__)
-  app.config['SECRET_KEY'] = 'hello world'
+  app.config['SECRET_KEY'] = 'asjkndajksnd ajsdnajksndkjan'
+
+  from .views import views
+  from .auth import auth
+
+  app.register_blueprint(views, url_prefix='/')
+  app.register_blueprint(auth, url_prefix='/')
 
   return app
